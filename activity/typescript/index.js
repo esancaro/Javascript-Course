b2aa3efe98d81d9d233290eb53f6fb2f5c63f862
esancaro <esancaro@gmail.com>
2022-07-12 23:45:09 -0600

    solución actividad sesión 3
    detalle solución 3 - typescript

--- activity/typescript/index.js
+++ activity/typescript/index.js
@@ -1,17 +1,68 @@
 // Ejercicio 1
+console.log("%cEjercicio 1", "color: green; font-size: 20px");
+const personFunc = (persona) => {
+    return Object.values(persona);
+};
+console.log(personFunc({ name: "Esteban", age: 47, profession: "Dev" }));
 // Ejercicio 2
-function sumOrConcatenate(value1, value2) {
-    if (typeof value1 === "number" && typeof value2 === "number") {
-        return value1 + value2;
-    }
-    else if (typeof value1 === "string" && typeof value2 === "string") {
-        return "".concat(value1, "-").concat(value2);
+console.log("%cEjercicio 2", "color: green; font-size: 20px");
+function sumOrConcatenate(param1, param2) {
+    // number + number
+    if (typeof param1 === "number" && typeof param2 === "number")
+        return param1 + param2; // number:string || string:number
+    if ((typeof param1 === "number" && typeof param2 === "string") ||
+        (typeof param1 === "string" && typeof param2 === "number"))
+        return `${param1}:${param2}`; // string-string
+    return `${param1}-${param2}`;
+}
+console.log(sumOrConcatenate(4, 3)); // 7
+console.log(sumOrConcatenate(4, "hello")); // 4:hello
+console.log(sumOrConcatenate("hello", 3)); // hello:7
+console.log(sumOrConcatenate("hello", "world")); //hello-world
+// Ejercicio 3
+console.log("%cEjercicio 3", "color: green; font-size: 20px");
+// type predicate
+const isCar = (vehicle) => {
+    return vehicle.pressPedal != undefined;
+};
+const vehicleFunc = (vehicle) => {
+    vehicle.turnOnEngine();
+    if (isCar(vehicle)) {
+        vehicle.pressPedal();
     }
     else {
-        return "".concat(value1, ":").concat(value2);
+        vehicle.openThrottle();
     }
-}
-console.log(sumOrConcatenate(4, 3));
-console.log(sumOrConcatenate(4, "hello"));
-console.log(sumOrConcatenate("hello", "world"));
-// Ejercicio 3
+};
+vehicleFunc(
+// Car
+{
+    tires: 4,
+    turnOnEngine: () => {
+        console.log("car turning on engine");
+    },
+    pressPedal: () => {
+        console.log("car pressing pedal");
+    },
+});
+vehicleFunc(
+// Motorcycle
+{
+    tires: 2,
+    turnOnEngine: () => {
+        console.log("Motorcycle turning on engine");
+    },
+    openThrottle: () => {
+        console.log("Motorcycle pressing pedal");
+    },
+});
+// Ejercicio 4
+console.log("%cEjercicio 4", "color: green; font-size: 20px");
+const arrayRemoveFirstElm = (x) => {
+    x.shift();
+    return x;
+};
+console.log(arrayRemoveFirstElm([1, 2, 3]));
+console.log(arrayRemoveFirstElm([1, 2, 3, "4"]));
+console.log(arrayRemoveFirstElm(["1", "2", "3", "4"]));
+// console.log(arrayRemoveFirstElm([1, {name: 'Esteban', surname: 'Castro'}, 3, '4']));
